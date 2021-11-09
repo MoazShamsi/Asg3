fa842cfce8b4caa7528da669230c98efdf488432
MoazShamsi <bcsf18m020@pucit.edu.pk>
2021-11-09 18:45:22 +0500

    designing quiz

--- app/src/main/java/com/example/asg3/quiz.java
+++ app/src/main/java/com/example/asg3/quiz.java
@@ -7,12 +7,12 @@ import android.widget.RadioButton;
 
 public class quiz extends AppCompatActivity {
 
-    RadioButton rd;
+    //RadioButton rd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        rd=(RadioButton) findViewById(R.id.radioButton2);
-        rd.setText("WHY ARE YOU CHANGING MY TEXT TELL ME RIGHT NOW PLEASE OR ELSE");
+        //rd=(RadioButton) findViewById(R.id.radioButton2);
+        //rd.setText("WHY ARE YOU CHANGING MY TEXT TELL ME RIGHT NOW PLEASE OR ELSE");
     }
 }
