8eb1eec447e49f662a4feff01568c2ff9327e2d3
MoazShamsi <bcsf18m020@pucit.edu.pk>
2021-11-09 18:21:47 +0500

    QUIZ INTENT

--- app/src/main/java/com/example/asg3/MainActivity.java
+++ app/src/main/java/com/example/asg3/MainActivity.java
@@ -9,16 +9,24 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button learn;
+    private Button learn,quiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         learn=(Button) findViewById(R.id.learnBtn);
+        quiz=(Button) findViewById(R.id.quizBtn);
         learn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,quiz.class);
                 startActivity(intent);
             }
         });
