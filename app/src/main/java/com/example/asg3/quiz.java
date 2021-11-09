f2a2cd8a22517b04d194fd0d65e0dfda09f0de4f
MoazShamsi <bcsf18m020@pucit.edu.pk>
2021-11-09 19:16:09 +0500

    adding ques to design

--- app/src/main/java/com/example/asg3/quiz.java
+++ app/src/main/java/com/example/asg3/quiz.java
@@ -4,15 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class quiz extends AppCompatActivity {
 
-    //RadioButton rd;
+    List<String> ques=new ArrayList<String>();
+    List<String> opt=new ArrayList<String>();
+    TextView qs;
+    RadioButton op1,op2,op3,op4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        //rd=(RadioButton) findViewById(R.id.radioButton2);
-        //rd.setText("WHY ARE YOU CHANGING MY TEXT TELL ME RIGHT NOW PLEASE OR ELSE");
+        ques.add("FROM WHICH PORTION OF MOUTH ل SOUND IS PRODUCED");
+        opt.add("Rounded tip of the tongue touching the base of the frontal 8 teeth");
+        opt.add("Rounded tip of the tongue and some portion near it touching the base of the frontal 4 teeth");
+        opt.add("Tongue touching center of mouth roof");
+        opt.add("behind the uvula");
+        qs=(TextView) findViewById(R.id.ques);
+        qs.setText(ques.get(0));
+
+        op1=(RadioButton) findViewById(R.id.opt1);
+        op2=(RadioButton) findViewById(R.id.opt2);
+        op3=(RadioButton) findViewById(R.id.opt3);
+        op4 =(RadioButton) findViewById(R.id.opt4);
+
+        op1.setText(opt.get(0));
+        op2.setText(opt.get(1));
+        op3.setText(opt.get(2));
+        op4.setText(opt.get(3));
+
     }
 }
