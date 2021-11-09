e1435c08495129f34e00312ebc0b9581754989ee
MoazShamsi <bcsf18m020@pucit.edu.pk>
2021-11-09 18:34:22 +0500

    CHANGING TEXT RADIO BTN

--- app/src/main/java/com/example/asg3/quiz.java
+++ app/src/main/java/com/example/asg3/quiz.java
@@ -3,12 +3,16 @@ package com.example.asg3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class quiz extends AppCompatActivity {
 
+    RadioButton rd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        rd=(RadioButton) findViewById(R.id.radioButton2);
+        rd.setText("WHY ARE YOU CHANGING MY TEXT TELL ME RIGHT NOW PLEASE OR ELSE");
     }
 }
